f05651ab0f890e1565ba737b29610ac711329b82
George-coder-tech <oppongboatenggeorge9@gmail.com>
2026-04-19 11:11:10 -0500

    Update bell_state_experiment.py

--- experiment/bell_state_experiment.py
+++ experiment/bell_state_experiment.py
@@ -15,6 +15,25 @@ qc.measure([0, 1], [0, 1])
 simulator = AerSimulator()
 result = simulator.run(qc, shots=1024).result()
 
-counts = result.get_counts()
+counts_ideal = result.get_counts()
 
-print("Results:", counts)
+print("Ideal Results:", counts_ideal)
+
+from qiskit_aer.noise import NoiseModel, depolarizing_error
+
+noise_model = NoiseModel()
+error = depolarizing_error(0.05, 1)
+noise_model.add_all_qubit_quantum_error(error, ['u1', 'u2', 'u3'])
+
+noisy_simulator = AerSimulator(noise_model=noise_model)
+result_noisy = noisy_simulator.run(qc, shots=1024).result()
+counts_noisy = result_noisy.get_counts()
+
+print("Noisy Results:", counts_noisy)
+
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+plt.show()
+plot_histogram([counts_ideal, counts_noisy], legend=["Ideal", "Noisy"])
+
+
